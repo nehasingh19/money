6055fd9f53879e8ddbcf957d4dfb06bf712aa13b
Ganesh K <mndlgen@gmail.com>
2018-03-05 09:20:18 +0530

    chnaged files

--- footer.php
+++ footer.php
@@ -1,28 +1,26 @@
-
 <section id="contact">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 mx-auto text-center">
                 <h2 class="section-heading">Let's Get In Touch!</h2>
                 <hr class="my-4">
-                <p class="mb-5">krishu love u jaan
+                <p class="mb-5">Help us to make world better place.
                 </p>
             </div>
         </div>
         <div class="row">
             <div class="col-lg-4 ml-auto text-center">
                 <i class="fa fa-phone fa-3x mb-3 sr-contact"></i>
-                <p>9410086959</p>
+                <p>+91 900 1234 1234</p>
             </div>
             <div class="col-lg-4 mr-auto text-center">
                 <i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
                 <p>
-                    <a href="mailto:your-email@your-domain.com">krishu94100@gmail.com</a>
+                    <a href="mailto:your-email@your-domain.com">author@mail.com</a>
                 </p>
             </div>
         </div>
     </div>
 </section>
 </body>
-
 </html>
--- header.php
+++ header.php
@@ -2,11 +2,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
     <title><?php get_bloginfo('name');?></title>
-
     <!-- Bootstrap core CSS -->
     <link href="<?php echo get_template_directory_uri() .'/css/bootstrap.css'; ?>" rel="stylesheet">
 <link href="<?php echo get_template_directory_uri().'/css/font-awesome.css'; ?>" rel="stylesheet"/>
@@ -25,100 +21,4 @@
 </head>
 
 <body id="page-top">
-
-<img src="<?php echo get_template_directory_uri().'/images/baby2.jpg' ?>"/>
-
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-
-
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#services">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<div class="flexslider carousel">
-    <ul class="slides">
-        <li>
-         <div id="slider" style="background-image: url('<?php //bloginfo('template_url'); ?>images\baby..jpg')">
-<!--            <img src="slide1.jpg" />-->-->
-        </li>
-        <li>
-            <img src="<?php echo get_template_directory_uri().'/images/baby2.jpg' ?>" height="100"width="100"/>
-            <img src="<?php echo get_template_directory_uri().'/images/baby3.jpg' ?>"/>
-<!--            <img src="slide2.jpg" />-->
-        </li>
-        <li>
-            <div id="slider" style="background-image: url('<?php //bloginfo('template_url'); ?>/*/C:\xampp\htdocs\wordpress\wp-content\themes\neha\images\baby3.jpg')">
-<!--            <img src="slide3.jpg" />-->
-        </li>
-        <li>
-<!--            <img src="slide4.jpg" />-->
-        </li>
-        <!-- items mirrored twice, total of 12 -->
-    </ul>
-</div>
-
-
-
-<section id="services">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <h2 class="section-heading">At Your Service</h2>
-                <hr class="my-4">
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-6 text-center">
-                <div class="service-box mt-5 mx-auto">
-                    <i class="fa fa-4x fa-diamond text-primary mb-3 sr-icons"></i>
-                    <h3 class="mb-3">Sturdy Templates</h3>
-                    <p class="text-muted mb-0">Our templates are updated regularly so they don't break.</p>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 text-center">
-                <div class="service-box mt-5 mx-auto">
-                    <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i>
-                    <h3 class="mb-3">Ready to Ship</h3>
-                    <p class="text-muted mb-0">You can use this theme as is, or you can make changes!</p>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 text-center">
-                <div class="service-box mt-5 mx-auto">
-                    <i class="fa fa-4x fa-newspaper-o text-primary mb-3 sr-icons"></i>
-                    <h3 class="mb-3">Up to Date</h3>
-                    <p class="text-muted mb-0">We update dependencies to keep things fresh.</p>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 text-center">
-                <div class="service-box mt-5 mx-auto">
-                    <i class="fa fa-4x fa-heart text-primary mb-3 sr-icons"></i>
-                    <h3 class="mb-3">Made with Love</h3>
-                    <p class="text-muted mb-0">You have to make your websites with love these days!</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+<?php wp_nav_menu( array( 'theme_location' => 'primary-menu' ) ); ?>
